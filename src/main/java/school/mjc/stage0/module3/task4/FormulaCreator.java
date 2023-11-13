a9ea406b7c5ef2b49f27973c4de755dda76976b4
sergei-zhirnov <s.zhirnov@protonmail.com>
2023-11-13 19:53:40 +0400

    M3T4 Try 2

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        int a = 4;
+        int a = 3;
         int b = 2;
         int formula = (9 * (a * a) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
 
